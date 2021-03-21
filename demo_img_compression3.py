6386b2e22174eca17ad506110cc3f936216bd855
thaiph99 <thaiph99@gmail.com>
2021-03-22 00:19:23 +0700

    update code

--- demo_img_compression3.py
+++ demo_img_compression3.py
@@ -10,8 +10,9 @@ class NpEncoder(json.JSONEncoder):
         if isinstance(obj, np.integer):
             return int(obj)
         elif isinstance(obj, np.floating):
-            return float(obj)
+            return "{:.4f}".format(obj)
         elif isinstance(obj, np.ndarray):
+            obj = np.round(obj, 4)
             return obj.tolist()
         else:
             return super(NpEncoder, self).default(obj)
@@ -19,13 +20,15 @@ class NpEncoder(json.JSONEncoder):
 
 def main():
 
-    path1 = 'datajpg/'
-    path2 = 'datapng/'
+    path1 = 'imagejpg/'
+    path2 = 'imagepng/'
+    path3 = 'imagejpeg/'
+    path4 = 'imagejpeg2/'
     f1 = '.jpg'
     f2 = '.png'
+    f3 = '.jpeg'
 
-    path, f = path2, f2
-    print(path+'imgg_origin'+f)
+    path, f = path1, f1
     img_origin = cv2.imread(path+'imgg_origin'+f, 1)
 
     # split matrix image
@@ -37,28 +40,35 @@ def main():
     with open(path+'imgj_origin.json', 'w') as f:
         json.dump(img_origin, f, cls=NpEncoder)
 
-    def compress_img(k, img):
+    def compress_img(img, percen):
+
+        pca = PCA().fit(img)
+        var_cumu = np.cumsum(pca.explained_variance_ratio_)*100
+        k = np.argmax(var_cumu > percen)
+
         ipca = IncrementalPCA(n_components=k)
         img_compressed = ipca.fit_transform(img)
         list_att = ['components_', 'mean_', 'explained_variance_', 'whiten']
         ipca_att = {}
         for att in list_att:
-            if type(np.array([])) == type(ipca.__getattribute__(att)):
-                att_values = np.round(ipca.__getattribute__(att), 4)
-            else:
-                att_values = ipca.__getattribute__(att)
+            # if type(np.array([])) == type(ipca.__getattribute__(att)):
+            #     att_values = np.round(ipca.__getattribute__(att), 4)
+            # else:
+            #     att_values = ipca.__getattribute__(att)
             # ipca_att[att] = ipca.__getattribute__(att)
-            ipca_att[att] = att_values
+            # print(att_values)
+            ipca_att[att] = ipca.__getattribute__(att)
 
-        return img_compressed, ipca_att
+        return k, img_compressed, ipca_att
 
-    def extract_img(k, result_compressed):
+    def extract_img(result_compressed):
+        k = result_compressed[0]
         ipca = IncrementalPCA(n_components=k)
-        img_compressed = result_compressed[0]
-        dict_att = result_compressed[1]
+        img_compressed = result_compressed[1]
+        dict_att = result_compressed[2]
         for key in dict_att.keys():
             ipca.__setattr__(key, dict_att[key])
-
+        # print((dict_att['mean_']))
         img_extracted = ipca.inverse_transform(img_compressed)
         return img_extracted
 
@@ -75,8 +85,10 @@ def main():
         plt.show()
 
     img_s = [img0, img1, img2]
-    n_components = 100
-    img_compressed = [compress_img(n_components, img_tmp) for img_tmp in img_s]
+    percent = 99.5
+
+    img_compressed = [compress_img(img_tmp, percent) for img_tmp in img_s]
+
     # img_compressed = {}
     # for i in range(len(img_s)):
     #     img_com = compress_img(n_components, img_s[i])
@@ -84,8 +96,10 @@ def main():
     # print(type(img_compressed))
 
     print('--------------------------------------------------')
-    tmp = img_compressed[0][1]
-    print(img_compressed[0][0].shape)
+    for i in range(3):
+        print('k'+str(i)+' : ', img_compressed[i][0])
+    tmp = img_compressed[0][2]
+    print(img_compressed[0][1].shape)
     print(tmp['components_'].shape)
     print(tmp['mean_'].shape)
     print(tmp['explained_variance_'].shape)
@@ -93,6 +107,7 @@ def main():
     print('--------------------------------------------------')
 
     # write json file
+    print(path)
     with open(path+'imgj_compressed.json', 'w') as f:
         json.dump(img_compressed, f, cls=NpEncoder)
 
@@ -102,13 +117,17 @@ def main():
         img_compressed1 = json.load(f)
 
     # extract image
-    img_extracted = [extract_img(n_components, img_tmp)
+    img_extracted = [extract_img(img_tmp)
                      for img_tmp in img_compressed1]
     img_extracted = concat_img(*img_extracted)
     print(img_extracted.shape)
-
+    fname = path+'imgg_extracted.jpg'
     # export image
-    fname = path+'imgg_extracted.png'
+    if f == '.png':
+        fname = path+'imgg_extracted.png'
+    if f == '.jpeg':
+        fname = path+'imgg_extracted.jpeg'
+
     cv2.imwrite(fname, img_extracted)
     print('Done')
 
